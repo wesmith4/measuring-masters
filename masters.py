be57afba9d2eb3eb9a321806274fa95f284a42ad
Will Smith <william.e.smithiv@gmail.com>
2021-04-08 12:17:08 -0400

    titles and rearrangements

--- masters.py
+++ masters.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import statsmodels.api as sm
 
-
+st.set_page_config(page_title="The Masters")
 st.title("Measuring the Masters")
 st.image("https://www.masters.com/assets/images/nav/masters_logo.png")
 
@@ -17,13 +17,6 @@ audio_file = open("./mastersTheme.mp3", "rb")
 audio_bytes = audio_file.read()
 st.audio(audio_bytes,format="audio/mp3")
 
-
-st.markdown("# Data Viewer")
-year = st.number_input("Year",min_value=1934,max_value=2021,value=2021)
-displayData = pd.read_csv("./data/{}data.csv".format(year))
-st.dataframe(displayData)
-
-
 # Winning Scores
 winningScores = pd.DataFrame(columns=("Year","Winning Score","Winner"))
 for year in range(1934,2022):
@@ -33,7 +26,7 @@ for year in range(1934,2022):
         winner = data["Player"].loc[0]
         winningScores.loc[year] = [year,winningScore,winner]
 
-
+st.markdown("## Winning Scores over the Years")
 fig = px.scatter(winningScores,
                 x=winningScores.Year,
                  y=winningScores["Winning Score"],
@@ -41,9 +34,15 @@ fig = px.scatter(winningScores,
                 labels={'x':'Year','y':'Winning Score'},
                 color_discrete_sequence=["#207F50"],
                 hover_name="Year",
-                hover_data=["Winner","Winning Score"],
-                title="Winning Scores over the Years")
+                hover_data=["Winner","Winning Score"])
 st.write(fig)
+
+
+st.markdown("# Data Viewer")
+year = st.number_input("Year",min_value=1934,max_value=2021,value=2021)
+displayData = pd.read_csv("./data/{}data.csv".format(year))
+st.dataframe(displayData)
+
 
 st.markdown("""
 [Visit the Masters site](https://masters.com)
